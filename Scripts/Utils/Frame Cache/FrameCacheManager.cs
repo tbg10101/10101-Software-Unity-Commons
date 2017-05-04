e77675881f5cd1e8959de331760c78c0704c3afa
Tristan Bellman-Greenwood <tbg10101@gmail.com>
2017-05-04 00:46:26 -0400

    updated the frame cache to remove stale caches without setting a stale-ness limit

--- Scripts/Utils/Frame Cache/FrameCacheManager.cs
+++ Scripts/Utils/Frame Cache/FrameCacheManager.cs
@@ -2,9 +2,20 @@
 using UnityEngine;
 
 namespace Software10101.Util {
+	/// <summary>
+	/// Tracks the stale-ness of individual <see cref="FrameCache"> objects.
+	/// 
+	/// Ensure that this runs before all scripts which use <see cref="FrameCache"> objects in the Unity execution order setting.
+	/// </summary>
 	public class FrameCacheManager : MonoBehaviour {
+		/// <summary>
+		/// The set of <see cref="FrameCache"> objects that are being tracked for stale-ness. <see cref="FrameCache{T}"> objects that are stale for 2 frames in-a-row are removed automatically.
+		/// </summary>
 		public static HashSet<FrameCache> frameCaches = new HashSet<FrameCache>();
 
+		/// <summary>
+		/// Whether or not the object should ersist between scene changes. Only one <see cref="FrameCacheManager"> will exist at any time, regarless of this selection.
+		/// </summary>
 		public bool dontDestroyOnLoad = false;
 
 		private FrameCacheManager instance = null;
@@ -23,7 +34,7 @@ namespace Software10101.Util {
 		} 
 
 		private void Update () {
-			frameCaches.ForEach(fc => fc.staleness++);
+			frameCaches.ForEach(fc => fc.Stale = true);
 		}
 	}
 }
