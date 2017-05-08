318404b8fcab13eaaf9b301868626537d5f6d980
Tristan Bellman-Greenwood <tbg10101@gmail.com>
2017-05-07 21:38:54 -0400

    switching to camel-case for Vector3 extensions since that is what Vector3 uses

--- Scripts/UnityEngine/Vector3Extensions.cs
+++ Scripts/UnityEngine/Vector3Extensions.cs
@@ -1,6 +1,6 @@
 ﻿namespace UnityEngine {
 	public static class Vector3Extensions {
-		public static Vector3 LimitMagnitude (this Vector3 v, float maxMagnitude) {
+		public static Vector3 limitMagnitude (this Vector3 v, float maxMagnitude) {
 			if (v.sqrMagnitude <= maxMagnitude * maxMagnitude) {
 				return v;
 			}
