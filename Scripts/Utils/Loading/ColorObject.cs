a67fcf80561976592720677c1f6ee189a87da9bb
Tristan Bellman-Greenwood <tbg10101@gmail.com>
2017-05-13 23:51:27 -0400

    adding a way to explicitly load named colors into the load helper

---
+++ Scripts/Utils/Loading/ColorObject.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+namespace Software10101.Utils.Loading {
+	public sealed class ColorObject : MonoBehaviour {
+		public Color Color;
+	}
+}
--- Scripts/Utils/Loading/LoadingHelper.cs
+++ Scripts/Utils/Loading/LoadingHelper.cs
@@ -1,28 +1,33 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
-namespace Software10101.Utils {
+namespace Software10101.Utils.Loading {
 	[ExecuteInEditMode]
 	public sealed class LoadingHelper : MonoBehaviour {
 		public List<Material> MaterialInputs;
+		public List<ColorObject> ColorsInputs;
 		public List<Texture2D> TextureInputs;
+		public List<Sprite> SpriteInputs;
 		public List<GameObject> PrefabInputs;
 
 		public static Dictionary<string, Material> Materials = new Dictionary<string, Material>();
 		public static Dictionary<string, Color> Colors = new Dictionary<string, Color>();
 		public static Dictionary<string, Texture2D> Textures = new Dictionary<string, Texture2D>();
+		public static Dictionary<string, Sprite> Sprites = new Dictionary<string, Sprite>();
 		public static Dictionary<string, GameObject> Prefabs = new Dictionary<string, GameObject>();
 
 #if UNITY_EDITOR
 		private void Update () {
-			LinkedHashSet<Material> materialSet = new LinkedHashSet<Material>(MaterialInputs);
-			if (materialSet.Count != MaterialInputs.Count) {
+			LinkedHashSet<string> materialNameSet = new LinkedHashSet<string>();
+
+			MaterialInputs.ForEach(element => materialNameSet.Add(element.name));
+
+			if (materialNameSet.Count != MaterialInputs.Count) {
 				Debug.LogError("An item with the same name already exists in this list.");
+				UnityEditor.Undo.PerformUndo();
 			}
-			MaterialInputs = materialSet.ToList();
 		}
-#else
+//#else
 		private void Awake () {
 			Materials.Clear();
 			Colors.Clear();
@@ -40,8 +45,16 @@ namespace Software10101.Utils {
 				Textures[m.name] = t;
 			}
 
+			foreach (ColorObject c in ColorsInputs) {
+				Colors[c.name] = c.Color;
+			}
+
 			foreach (Texture2D t in TextureInputs) {
 				Textures[t.name] = t;
+			}
+
+			foreach (Sprite s in SpriteInputs) {
+				Sprites[s.name] = s;
 			}
 
 			foreach (GameObject go in PrefabInputs) {
