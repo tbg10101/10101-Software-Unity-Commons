35ff651aa9132059e7119a2fcf4dd5d813da0f93
Tristan Bellman-Greenwood <btristan@amazon.com>
2018-09-15 19:05:33 -0600

    fixed some formatting

--- Scripts/System/Objects.cs
+++ Scripts/System/Objects.cs
@@ -1,8 +1,10 @@
 ﻿namespace System {
 	public static class Objects {
-        static bool IsNullable(Type type) => type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>);
+        static bool IsNullable(Type type) {
+            return type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>);
+        }
 
-        public static T RequireNonNull<T>(T param) {
+        public static T RequireNonNull<T> (T param) {
             if (IsNullable(typeof(T)) && param == null) {
 				throw new ArgumentException();
 			}
@@ -10,15 +12,15 @@
 			return param;
 		}
 
-        public static int RoundUp(this double me) {
+        public static int RoundUp (this double me) {
             return (int)Math.Ceiling(me);
         }
 
-        public static int RoundDown(this double me) {
+        public static int RoundDown (this double me) {
             return (int)Math.Floor(me);
         }
 
-        public static int Round(this double me) {
+        public static int Round (this double me) {
             return (int)Math.Round(me);
         }
     }
