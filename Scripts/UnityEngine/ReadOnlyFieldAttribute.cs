0a177ade5793387df0344c560f9a7efc1beef69c
Tristan Bellman-Greenwood <tbg10101@gmail.com>
2018-10-14 01:13:27 -0400

    adding a read-onlt field attribute

---
+++ Scripts/UnityEngine/ReadOnlyFieldAttribute.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEditor;
+
+namespace UnityEngine {
+	[AttributeUsage(AttributeTargets.Field)]
+	public class ReadOnlyFieldAttribute : PropertyAttribute { }
+
+	[CustomPropertyDrawer(typeof(ReadOnlyFieldAttribute))]
+	public class ReadOnlyDrawer : PropertyDrawer {
+		public override float GetPropertyHeight (
+			SerializedProperty property,
+			GUIContent label) {
+			return EditorGUI.GetPropertyHeight(property, label, true);
+		}
+
+		public override void OnGUI (
+			Rect position,
+			SerializedProperty property,
+			GUIContent label) {
+			GUI.enabled = false;
+			EditorGUI.PropertyField(position, property, label, true);
+			GUI.enabled = true;
+		}
+	}
+}
