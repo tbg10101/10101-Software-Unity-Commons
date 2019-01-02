edbe9d1c0cc694ff3be9d473703ee37cd29a675a
Tristan Bellman-Greenwood <tbg10101@gmail.com>
2019-01-02 00:46:33 -0500

    adding editor extensions, refactoring logging script assembly, adding meta file (just to try it out)

---
+++ Scripts/UnityEngine/Editor/EditorExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace UnityEditor {
+    public static class EditorExtensions {
+        /// <summary>
+        /// Gets the direct children of a SerializedProperty. (it does not recurse into children)
+        ///
+        /// Source: https://forum.unity.com/threads/loop-through-serializedproperty-children.435119/#post-2814895
+        /// </summary>
+        /// <param name="property"></param>
+        /// <returns></returns>
+        public static IEnumerable<SerializedProperty> GetChildren (this SerializedProperty property) {
+            property = property.Copy();
+            SerializedProperty nextElement = property.Copy();
+
+            bool hasNextElement = nextElement.NextVisible(false);
+            if (!hasNextElement) {
+                nextElement = null;
+            }
+
+            property.NextVisible(true);
+            while (true) {
+                if ((SerializedProperty.EqualContents(property, nextElement))) {
+                    yield break;
+                }
+
+                yield return property;
+
+                bool hasNext = property.NextVisible(false);
+                if (!hasNext) {
+                    break;
+                }
+            }
+        }
+    }
+}
